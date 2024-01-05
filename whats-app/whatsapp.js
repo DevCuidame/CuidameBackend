99ba196e7f86188cdd3d0e0c369f46998dada242
AnthonyLopezz <anthony.lopez@usantoto.edu.co>
2024-01-05 17:26:16 -0500

    Fix(Whatsapp): added --no-sandbox args

--- whats-app/whatsapp.js
+++ whats-app/whatsapp.js
@@ -1,25 +1,29 @@
 // whatsapp-config.js
-const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth  } = require('whatsapp-web.js');
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 
-const client = new Client();
+const puppeteerOptions = {
+  puppeteer: {
+    args: ["--no-sandbox"],
+  },
+};
+const client = new Client({ puppeteer: puppeteerOptions });
 
-client.on('qr', qr => {
-    // Generate and scan this QR code with your phone
-    qrcode.generate(qr, {small: true});
+client.on("qr", (qr) => {
+  // Generate and scan this QR code with your phone
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
 
-client.on('message', message => {
-    if(message.body === '!ping') {
-        client.sendMessage(message.from, 'pong');
-        //client.sendMessage("573194843592@c.us", 'pong');
-    }
+client.on("message", (message) => {
+  if (message.body === "!ping") {
+    client.sendMessage(message.from, "pong");
+    //client.sendMessage("573194843592@c.us", 'pong');
+  }
 });
 
 client.initialize();
-
 module.exports = { client };
