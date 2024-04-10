b11c7f471ce648f1147406b84b3a91011a10d02e
AnthonyLopezz <anthony.lopez@usantoto.edu.co>
2024-04-10 10:54:34 -0500

    fix(whatsapp)

--- whats-app/whatsapp.js
+++ whats-app/whatsapp.js
@@ -1,5 +1,6 @@
 const qrcode = require("qrcode-terminal");
 const { Client, LocalAuth } = require("whatsapp-web.js");
+const wwebVersion = '2.2407.3';
 
 const client = new Client({
   puppeteer: {
@@ -8,7 +9,10 @@ const client = new Client({
       "--disable-setuid-sandbox",
     ],
   },
-  authStrategy: new LocalAuth(),
+  webVersionCache: {
+    type: 'remote',
+    remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+},
 });
 
 client.on("qr", (qr) => {
@@ -17,18 +21,14 @@ client.on("qr", (qr) => {
 
 client.on("ready", () => {
   console.log("Client is ready!");
-  const number = "+573194843592";
-    const text = "Hey An";
-    const chatId = number.substring(1) + "@c.us";
-    client.sendMessage(chatId, text);
 });
-
+ 
 client.on("message", (message) => {
   if (message.body === "!ping") {
     client.sendMessage(message.from, "pong");
   }
 });
 
-client.initialize();
+client.initialize();  
 
 module.exports = { client };
