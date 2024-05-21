ca39464ca04203830b1dc3e7a53d17fe76acde10
AnthonyLopezz <anthony.lopez@usantoto.edu.co>
2024-05-21 13:50:26 -0500

    Feat(Wompi payment Component): Component added to testing the data encrypt and integrity key response from backend

--- controllers/wompi/wompiController.js
+++ controllers/wompi/wompiController.js
@@ -1,28 +1,20 @@
-
 async function encryptData(req, res) {
   try {
-    const reference = req.body.reference;
-    const amount = req.body.amount;
-    const currency = req.body.currency;
-    const expirationDate = req.body.expirationDate;
+    const { reference, amount, currency, expirationDate } = req.body;
     const secretIntegrity = process.env.TEST_INTEGRITY;
 
-    var data = reference + amount + currency + expirationDate + secretIntegrity;
-    console.log("🚀 ~ encryptData ~ data:", data)
-
+    const data = reference + amount + currency + expirationDate + secretIntegrity;
     const encondedText = new TextEncoder().encode(data);
     const hashBuffer = await crypto.subtle.digest("SHA-256", encondedText);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray
-      .map((b) => b.toString(16).padStart(2, "0"))
-      .join("");
-    return res.status(200).json(hashHex);
+    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+
+    return res.status(200).json({ integrity: hashHex });
   } catch (error) {
-    console.log("🚀 ~ encryptData ~ error:", error)
-    return res.status(501).json({
+    return res.status(400).json({
       success: false,
-      message: "Hubo un error con la encriptación de la data.",
-      error: error,
+      message: "Hubo un error con la encriptación de la información.",
+      error: error.message,
     });
   }
 }
--- routes/wompi/Wompi.router.js
+++ routes/wompi/Wompi.router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const wompiController  = require("../../controllers/wompi/wompiController");
 
-router.post("/", wompiController.encryptData);
+router.post("", wompiController.encryptData);
 
 module.exports = router;
